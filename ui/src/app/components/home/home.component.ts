383ca5ef2ec988013b8f5501f519675b26cf08d1
Vivek Yadav <vivek.yadav@ollion.com>
2024-12-09 19:25:44 +0530

    resolve the comment

--- ui/src/app/components/home/home.component.ts
+++ ui/src/app/components/home/home.component.ts
@@ -22,7 +22,6 @@ export class HomeComponent implements OnInit {
      }
 
   ngOnInit(): void {
-
     this.data.isOffline.subscribe({
       next: (res: boolean) => {
         this.isOfflineStatus = res
